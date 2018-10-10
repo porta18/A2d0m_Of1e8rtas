eb47fbef23ebc586fade70b9a5e6a570e6870a3b
CarlosRavanalMillan <43883552+CarlosRavanalMillan@users.noreply.github.com>
2018-10-09 21:38:09 -0300

    Carlos
    pruebas

--- build/generated/jax-wsCache/Service1/org/tempuri/ObjectFactory.java
+++ build/generated/jax-wsCache/Service1/org/tempuri/ObjectFactory.java
@@ -25,22 +25,22 @@ import org.datacontract.schemas._2004._07.wsporta.CompositeType;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _GetDataUsingDataContractResponseGetDataUsingDataContractResult_QNAME = new QName("http://tempuri.org/", "GetDataUsingDataContractResult");
-    private final static QName _CreaUserPClave_QNAME = new QName("http://tempuri.org/", "pClave");
-    private final static QName _CreaUserPCorreo_QNAME = new QName("http://tempuri.org/", "pCorreo");
-    private final static QName _CreaUserPNombres_QNAME = new QName("http://tempuri.org/", "pNombres");
-    private final static QName _InsertPersResponseInsertPersResult_QNAME = new QName("http://tempuri.org/", "InsertPersResult");
-    private final static QName _GetDataResponseGetDataResult_QNAME = new QName("http://tempuri.org/", "GetDataResult");
-    private final static QName _GetSexoResponseGetSexoResult_QNAME = new QName("http://tempuri.org/", "getSexoResult");
-    private final static QName _GetDataUsingDataContractComposite_QNAME = new QName("http://tempuri.org/", "composite");
-    private final static QName _LoginUsuarioResponseLoginUsuarioResult_QNAME = new QName("http://tempuri.org/", "LoginUsuarioResult");
-    private final static QName _LoginUsuarioPNombre_QNAME = new QName("http://tempuri.org/", "pNombre");
-    private final static QName _LoginUsuarioPPassword_QNAME = new QName("http://tempuri.org/", "pPassword");
-    private final static QName _CreaUserResponseCreaUserResult_QNAME = new QName("http://tempuri.org/", "CreaUserResult");
-    private final static QName _UpdateUserPassPUsuario_QNAME = new QName("http://tempuri.org/", "pUsuario");
+    private final static QName _InsertPersPNombres_QNAME = new QName("http://tempuri.org/", "pNombres");
     private final static QName _InsertPersPAp_QNAME = new QName("http://tempuri.org/", "pAp");
     private final static QName _InsertPersPAm_QNAME = new QName("http://tempuri.org/", "pAm");
+    private final static QName _CreaUserResponseCreaUserResult_QNAME = new QName("http://tempuri.org/", "CreaUserResult");
+    private final static QName _LoginUsuarioPNombre_QNAME = new QName("http://tempuri.org/", "pNombre");
+    private final static QName _LoginUsuarioPPassword_QNAME = new QName("http://tempuri.org/", "pPassword");
+    private final static QName _GetDataUsingDataContractResponseGetDataUsingDataContractResult_QNAME = new QName("http://tempuri.org/", "GetDataUsingDataContractResult");
+    private final static QName _UpdateUserPassPCorreo_QNAME = new QName("http://tempuri.org/", "pCorreo");
+    private final static QName _UpdateUserPassPClave_QNAME = new QName("http://tempuri.org/", "pClave");
+    private final static QName _UpdateUserPassPUsuario_QNAME = new QName("http://tempuri.org/", "pUsuario");
+    private final static QName _GetDataUsingDataContractComposite_QNAME = new QName("http://tempuri.org/", "composite");
+    private final static QName _LoginUsuarioResponseLoginUsuarioResult_QNAME = new QName("http://tempuri.org/", "LoginUsuarioResult");
+    private final static QName _GetSexoResponseGetSexoResult_QNAME = new QName("http://tempuri.org/", "getSexoResult");
+    private final static QName _InsertPersResponseInsertPersResult_QNAME = new QName("http://tempuri.org/", "InsertPersResult");
     private final static QName _UpdateUserPassResponseUpdateUserPassResult_QNAME = new QName("http://tempuri.org/", "UpdateUserPassResult");
+    private final static QName _GetDataResponseGetDataResult_QNAME = new QName("http://tempuri.org/", "GetDataResult");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.tempuri
@@ -178,147 +178,12 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link CompositeType }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetDataUsingDataContractResult", scope = GetDataUsingDataContractResponse.class)
-    public JAXBElement<CompositeType> createGetDataUsingDataContractResponseGetDataUsingDataContractResult(CompositeType value) {
-        return new JAXBElement<CompositeType>(_GetDataUsingDataContractResponseGetDataUsingDataContractResult_QNAME, CompositeType.class, GetDataUsingDataContractResponse.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pClave", scope = CreaUser.class)
-    public JAXBElement<String> createCreaUserPClave(String value) {
-        return new JAXBElement<String>(_CreaUserPClave_QNAME, String.class, CreaUser.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pCorreo", scope = CreaUser.class)
-    public JAXBElement<String> createCreaUserPCorreo(String value) {
-        return new JAXBElement<String>(_CreaUserPCorreo_QNAME, String.class, CreaUser.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pNombres", scope = CreaUser.class)
-    public JAXBElement<String> createCreaUserPNombres(String value) {
-        return new JAXBElement<String>(_CreaUserPNombres_QNAME, String.class, CreaUser.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "InsertPersResult", scope = InsertPersResponse.class)
-    public JAXBElement<String> createInsertPersResponseInsertPersResult(String value) {
-        return new JAXBElement<String>(_InsertPersResponseInsertPersResult_QNAME, String.class, InsertPersResponse.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetDataResult", scope = GetDataResponse.class)
-    public JAXBElement<String> createGetDataResponseGetDataResult(String value) {
-        return new JAXBElement<String>(_GetDataResponseGetDataResult_QNAME, String.class, GetDataResponse.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "getSexoResult", scope = GetSexoResponse.class)
-    public JAXBElement<String> createGetSexoResponseGetSexoResult(String value) {
-        return new JAXBElement<String>(_GetSexoResponseGetSexoResult_QNAME, String.class, GetSexoResponse.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link CompositeType }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "composite", scope = GetDataUsingDataContract.class)
-    public JAXBElement<CompositeType> createGetDataUsingDataContractComposite(CompositeType value) {
-        return new JAXBElement<CompositeType>(_GetDataUsingDataContractComposite_QNAME, CompositeType.class, GetDataUsingDataContract.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "LoginUsuarioResult", scope = LoginUsuarioResponse.class)
-    public JAXBElement<String> createLoginUsuarioResponseLoginUsuarioResult(String value) {
-        return new JAXBElement<String>(_LoginUsuarioResponseLoginUsuarioResult_QNAME, String.class, LoginUsuarioResponse.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pNombre", scope = LoginUsuario.class)
-    public JAXBElement<String> createLoginUsuarioPNombre(String value) {
-        return new JAXBElement<String>(_LoginUsuarioPNombre_QNAME, String.class, LoginUsuario.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pPassword", scope = LoginUsuario.class)
-    public JAXBElement<String> createLoginUsuarioPPassword(String value) {
-        return new JAXBElement<String>(_LoginUsuarioPPassword_QNAME, String.class, LoginUsuario.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "CreaUserResult", scope = CreaUserResponse.class)
-    public JAXBElement<String> createCreaUserResponseCreaUserResult(String value) {
-        return new JAXBElement<String>(_CreaUserResponseCreaUserResult_QNAME, String.class, CreaUserResponse.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pCorreo", scope = UpdateUserPass.class)
-    public JAXBElement<String> createUpdateUserPassPCorreo(String value) {
-        return new JAXBElement<String>(_CreaUserPCorreo_QNAME, String.class, UpdateUserPass.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pClave", scope = UpdateUserPass.class)
-    public JAXBElement<String> createUpdateUserPassPClave(String value) {
-        return new JAXBElement<String>(_CreaUserPClave_QNAME, String.class, UpdateUserPass.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pUsuario", scope = UpdateUserPass.class)
-    public JAXBElement<String> createUpdateUserPassPUsuario(String value) {
-        return new JAXBElement<String>(_UpdateUserPassPUsuario_QNAME, String.class, UpdateUserPass.class, value);
-    }
-
-    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
      * 
      */
     @XmlElementDecl(namespace = "http://tempuri.org/", name = "pNombres", scope = InsertPers.class)
     public JAXBElement<String> createInsertPersPNombres(String value) {
-        return new JAXBElement<String>(_CreaUserPNombres_QNAME, String.class, InsertPers.class, value);
+        return new JAXBElement<String>(_InsertPersPNombres_QNAME, String.class, InsertPers.class, value);
     }
 
     /**
@@ -343,9 +208,144 @@ public class ObjectFactory {
      * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
      * 
      */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "CreaUserResult", scope = CreaUserResponse.class)
+    public JAXBElement<String> createCreaUserResponseCreaUserResult(String value) {
+        return new JAXBElement<String>(_CreaUserResponseCreaUserResult_QNAME, String.class, CreaUserResponse.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pNombre", scope = LoginUsuario.class)
+    public JAXBElement<String> createLoginUsuarioPNombre(String value) {
+        return new JAXBElement<String>(_LoginUsuarioPNombre_QNAME, String.class, LoginUsuario.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pPassword", scope = LoginUsuario.class)
+    public JAXBElement<String> createLoginUsuarioPPassword(String value) {
+        return new JAXBElement<String>(_LoginUsuarioPPassword_QNAME, String.class, LoginUsuario.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CompositeType }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetDataUsingDataContractResult", scope = GetDataUsingDataContractResponse.class)
+    public JAXBElement<CompositeType> createGetDataUsingDataContractResponseGetDataUsingDataContractResult(CompositeType value) {
+        return new JAXBElement<CompositeType>(_GetDataUsingDataContractResponseGetDataUsingDataContractResult_QNAME, CompositeType.class, GetDataUsingDataContractResponse.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pCorreo", scope = UpdateUserPass.class)
+    public JAXBElement<String> createUpdateUserPassPCorreo(String value) {
+        return new JAXBElement<String>(_UpdateUserPassPCorreo_QNAME, String.class, UpdateUserPass.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pClave", scope = UpdateUserPass.class)
+    public JAXBElement<String> createUpdateUserPassPClave(String value) {
+        return new JAXBElement<String>(_UpdateUserPassPClave_QNAME, String.class, UpdateUserPass.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pUsuario", scope = UpdateUserPass.class)
+    public JAXBElement<String> createUpdateUserPassPUsuario(String value) {
+        return new JAXBElement<String>(_UpdateUserPassPUsuario_QNAME, String.class, UpdateUserPass.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CompositeType }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "composite", scope = GetDataUsingDataContract.class)
+    public JAXBElement<CompositeType> createGetDataUsingDataContractComposite(CompositeType value) {
+        return new JAXBElement<CompositeType>(_GetDataUsingDataContractComposite_QNAME, CompositeType.class, GetDataUsingDataContract.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "LoginUsuarioResult", scope = LoginUsuarioResponse.class)
+    public JAXBElement<String> createLoginUsuarioResponseLoginUsuarioResult(String value) {
+        return new JAXBElement<String>(_LoginUsuarioResponseLoginUsuarioResult_QNAME, String.class, LoginUsuarioResponse.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "getSexoResult", scope = GetSexoResponse.class)
+    public JAXBElement<String> createGetSexoResponseGetSexoResult(String value) {
+        return new JAXBElement<String>(_GetSexoResponseGetSexoResult_QNAME, String.class, GetSexoResponse.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pClave", scope = CreaUser.class)
+    public JAXBElement<String> createCreaUserPClave(String value) {
+        return new JAXBElement<String>(_UpdateUserPassPClave_QNAME, String.class, CreaUser.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pCorreo", scope = CreaUser.class)
+    public JAXBElement<String> createCreaUserPCorreo(String value) {
+        return new JAXBElement<String>(_UpdateUserPassPCorreo_QNAME, String.class, CreaUser.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "pNombres", scope = CreaUser.class)
+    public JAXBElement<String> createCreaUserPNombres(String value) {
+        return new JAXBElement<String>(_InsertPersPNombres_QNAME, String.class, CreaUser.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "InsertPersResult", scope = InsertPersResponse.class)
+    public JAXBElement<String> createInsertPersResponseInsertPersResult(String value) {
+        return new JAXBElement<String>(_InsertPersResponseInsertPersResult_QNAME, String.class, InsertPersResponse.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
     @XmlElementDecl(namespace = "http://tempuri.org/", name = "UpdateUserPassResult", scope = UpdateUserPassResponse.class)
     public JAXBElement<String> createUpdateUserPassResponseUpdateUserPassResult(String value) {
         return new JAXBElement<String>(_UpdateUserPassResponseUpdateUserPassResult_QNAME, String.class, UpdateUserPassResponse.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetDataResult", scope = GetDataResponse.class)
+    public JAXBElement<String> createGetDataResponseGetDataResult(String value) {
+        return new JAXBElement<String>(_GetDataResponseGetDataResult_QNAME, String.class, GetDataResponse.class, value);
     }
 
 }
